a03f0db662a87a868638cc1a3730219a863c43c1
State Of Mind <robotics@tfanh.org>
2024-03-07 14:06:53 -0500

    Changed subsystem directory to gamepad directory for clarity.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/gamepad/CommandGamepad.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/gamepad/CommandGamepad.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.subsystems;
+package org.firstinspires.ftc.teamcode.gamepad;
 
 import com.arcrobotics.ftclib.command.SubsystemBase;
 import com.arcrobotics.ftclib.command.button.GamepadButton;
